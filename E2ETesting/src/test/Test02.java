2912825fa348de8774959497b6e28be571aee8ad
Scott Robb <jscottrobb@yahoo.com>
2017-09-28 22:12:25 -0600

    Initial commit

---
+++ E2ETesting/src/test/Test02.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.annotations.*;
+import org.openqa.selenium.chrome.*;
+
+public class Test02 {
+
+	@Test
+	public void testcase1 () {
+		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElementById("email").sendKeys("Hello");
+		driver.findElementById("pass").sendKeys("Hello");
+		driver.quit();
+	};
+}
